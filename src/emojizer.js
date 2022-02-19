c5532290ea26e0e57ddab7419bd3593b3169af46
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-02-19 17:19:51 +0530

    Advance Edit

--- src/emojizer.js
+++ src/emojizer.js
@@ -5,30 +5,54 @@ import ReactTooltip from "react-tooltip";
 import Switch from "react-switch";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const Emojizer = () => {
-  const [text, setText] = useState("");
-  const [emojized, setEmojized] = useState([]);
+  const [text, setText] = useState(
+    "I am in my car, confused about whether I will be on time. The train was delayed and I was running away from the police man It's ok now though. Please phone me later, maybe we can have a pizza tonight while watching tv"
+  );
+  const [emojized, setEmojized] = useState("");
   const [advanceMode, setadvanceMode] = useState(false);
   const [removeMode, setremoveMode] = useState(false);
   const [fontSize, setfontSize] = useState(20);
 
+  /**
+   * @description: This function is used to emojize the text
+   */
   const EmojizeText = () => {
     var words = [];
     if (text.length > 0) {
-      words = text.trim().replace(/,/, "").split(/\s+/);
+      words = text
+        .trim()
+        .replace(/[^0-9a-z ]/gi, "")
+        .split(/\s+/);
 
       //get words from words array and replace with emojis
       var emojizedWords = words.map((word) => {
-        var emoji = nodeEmoji.find(pluralize.singular(word));
+        var singularWord = pluralize.singular(word);
+        //make the word singular and find the excat matching emoji
+        var emoji = nodeEmoji.find(singularWord);
+
         if (emoji) {
-          return { word: word, emoji: emoji.emoji };
+          return {
+            word: word,
+            emoji: emoji.emoji,
+            advance: false,
+            emojiCount: 1,
+          };
         } else {
-          var similarEmoji = nodeEmoji.search(word);
+          var similarEmoji = nodeEmoji.search(singularWord);
           if (similarEmoji.length > 0 && advanceMode) {
-            return { word: word, emoji: similarEmoji.pop().emoji };
+            //return the last emoji if there are more than one
+            return {
+              word: word,
+              emoji: similarEmoji.pop().emoji,
+              advance: true,
+              emojiCount: similarEmoji.length,
+            };
           } else {
-            return { word: word, emoji: "" };
+            return { word: word, emoji: "", advance: false, emojiCount: 0 };
           }
         }
       });
@@ -39,12 +63,20 @@ const Emojizer = () => {
   const [selectedKey, setselectedKey] = useState("");
   const [currentIndex, setcurrentIndex] = useState(0);
 
+  /**
+   * @description This function is used to toggle between multiple emojis
+   * @param {*} key word array index
+   * @param {*} word word
+   */
   const ChangeEmoji = (key, word) => {
+    //get similar emojis
     var similarEmoji = nodeEmoji.search(pluralize.singular(word));
 
     if (similarEmoji.length > 0) {
+      //if changing the previous change emoji
       if (selectedKey === key) {
         if (currentIndex >= similarEmoji.length) {
+          //display the emoji word
           const newArray = [...emojized];
           newArray[key].emoji = "";
           setEmojized(newArray);
@@ -73,16 +105,36 @@ const Emojizer = () => {
     setremoveMode(nextChecked);
   };
 
+  /**
+   * @description: This function is used remove the selected emoji
+   * @param {*} key word array index
+   */
   const removeWord = (key) => {
     setEmojized(emojized.filter((word, index) => index !== key));
   };
 
+  /**
+   * @description: This function is copy the emojized text to clipboard
+   */
   function copyText() {
     var text = Object.entries(emojized).map((word, key) => {
       return word[1].emoji ? word[1].emoji : word[1].word;
     });
     navigator.clipboard.writeText(text.join(" "));
   }
+
+  const PopupExample = () => (
+    <Popup trigger={<button>Trigger</button>} position="top left">
+      {(close) => (
+        <div>
+          Content here
+          <a className="close" onClick={close}>
+            &times;
+          </a>
+        </div>
+      )}
+    </Popup>
+  );
 
   return (
     <div className="container">
@@ -91,6 +143,7 @@ const Emojizer = () => {
         <ReactTooltip effect="solid" />
         <div>
           <textarea
+            defaultValue={text}
             className="text-area"
             name="text"
             onChange={(e) => {
@@ -139,6 +192,7 @@ const Emojizer = () => {
           </div>
         </div>
         <hr className="hr-line" />
+        <PopupExample />
 
         <div className="output-container">
           <p style={{ fontSize: "12px" }} hidden={emojized.length === 0}>
@@ -155,7 +209,7 @@ const Emojizer = () => {
                   removeMode ? removeWord(key) : ChangeEmoji(key, word[1].word);
                 }}
                 style={{
-                  border: !removeMode ? "1px solid #c9c9c9" : "",
+                  border: word[1].advance ? "1px solid #a3cfab" : "",
                   fontSize: fontSize + "px",
                 }}
               >
