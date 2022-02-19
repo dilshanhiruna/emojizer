599b54ece26da09c53aa598249aff39b9567be84
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-02-19 12:14:39 +0530

    Change/Remove Emoji | Advance Mode

--- src/emojizer.js
+++ src/emojizer.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import nodeEmoji from "node-emoji";
 import pluralize from "pluralize";
+import ReactTooltip from "react-tooltip";
+import Switch from "react-switch";
 
 const Emojizer = () => {
   const [text, setText] = useState("");
-  const [emojized, setEmojized] = useState("");
+  const [emojized, setEmojized] = useState([]);
+  const [advanceMode, setadvanceMode] = useState(false);
+  const [removeMode, setremoveMode] = useState(false);
 
   const EmojizeText = () => {
     var words = [];
@@ -18,7 +22,7 @@ const Emojizer = () => {
           return { word: word, emoji: emoji.emoji };
         } else {
           var similarEmoji = nodeEmoji.search(word);
-          if (similarEmoji.length > 0) {
+          if (similarEmoji.length > 0 && advanceMode) {
             return { word: word, emoji: similarEmoji.pop().emoji };
           } else {
             return { word: word, emoji: "" };
@@ -28,6 +32,54 @@ const Emojizer = () => {
       setEmojized(emojizedWords);
     }
   };
+
+  const [selectedKey, setselectedKey] = useState("");
+  const [currentIndex, setcurrentIndex] = useState(0);
+
+  const ChangeEmoji = (key, word) => {
+    var similarEmoji = nodeEmoji.search(pluralize.singular(word));
+
+    if (similarEmoji.length > 0) {
+      if (selectedKey === key) {
+        if (currentIndex >= similarEmoji.length) {
+          const newArray = [...emojized];
+          newArray[key].emoji = "";
+          setEmojized(newArray);
+          setcurrentIndex(0);
+        } else {
+          const newArray = [...emojized];
+          newArray[key].emoji = similarEmoji[currentIndex].emoji;
+          setEmojized(newArray);
+          setcurrentIndex(currentIndex + 1);
+        }
+      } else {
+        const newArray = [...emojized];
+        newArray[key].emoji = similarEmoji[0].emoji;
+        setEmojized(newArray);
+
+        setcurrentIndex(currentIndex + 1);
+        setselectedKey(key);
+      }
+    }
+  };
+
+  const handleChangeAdvanceSwitch = (nextChecked) => {
+    setadvanceMode(nextChecked);
+  };
+  const handleChangeRemoveSwitch = (nextChecked) => {
+    setremoveMode(nextChecked);
+  };
+
+  const removeWord = (key) => {
+    setEmojized(emojized.filter((word, index) => index !== key));
+  };
+
+  function copyText() {
+    var text = Object.entries(emojized).map((word, key) => {
+      return word[1].emoji ? word[1].emoji : word[1].word;
+    });
+    navigator.clipboard.writeText(text.join(" "));
+  }
 
   return (
     <div>
@@ -42,18 +94,48 @@ const Emojizer = () => {
         }}
       ></textarea>
       <br />
+      <span>Advance mode</span>
+      <Switch onChange={handleChangeAdvanceSwitch} checked={advanceMode} />
+      <span>Remove mode</span>
+      <Switch
+        onChange={handleChangeRemoveSwitch}
+        checked={removeMode}
+        onColor="#ff0d1d"
+      />
+      <button onClick={copyText}>Copy</button>
       <button onClick={EmojizeText}>emojize</button>
       <br />
-
       <div style={{ display: "flex", justifyContent: "center" }}>
-        {Object.entries(emojized).map((word) => {
+        {Object.entries(emojized).map((word, key) => {
           return word[1].emoji ? (
-            <div className="word-box">{word[1].emoji}</div>
+            <div
+              className={
+                "word-box" + (removeMode ? " shake-slow word-box-remove" : "")
+              }
+              key={key}
+              onClick={() => {
+                removeMode ? removeWord(key) : ChangeEmoji(key, word[1].word);
+              }}
+              style={{ border: !removeMode ? "1px solid #c9c9c9" : "" }}
+            >
+              <span data-tip={word[1].word}> {word[1].emoji}</span>
+            </div>
           ) : (
-            <div className="word-box">{word[1].word}</div>
+            <div
+              className={
+                "word-box" + (removeMode ? " shake-slow word-box-remove" : "")
+              }
+              key={key}
+              onClick={() => {
+                removeMode ? removeWord(key) : ChangeEmoji(key, word[1].word);
+              }}
+            >
+              {word[1].word}
+            </div>
           );
         })}
       </div>
+      <ReactTooltip effect="solid" />;
     </div>
   );
 };
